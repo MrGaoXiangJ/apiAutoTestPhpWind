1e863b731cf492a82eadeb7c38380701057347cd
MrGaoXiangJ <13552002004@126.com>
2021-05-20 21:15:07 +0800

    处理文件依赖

--- case/test_login.py
+++ case/test_login.py
@@ -3,11 +3,16 @@
 """
 
 # 导包 unittest ApiLogin
+import json
 import unittest
+from time import sleep
+
 from api.api_login import ApiLogin
 from parameterized import parameterized
-from tools.read_json import ReadJson
 
+from tools.read_extract import ReadExtract
+from tools.read_json import ReadJson
+import re
 
 # 读取数据函数
 def get_data():
@@ -31,14 +36,23 @@ class TestLogin(unittest.TestCase):
         s = ApiLogin().api_post_login(jumpurl, step, pwuser, pwpwd, lgt)
 
         # 调试使用
-        print("查看相应结果:", s.reason)
-        print('文件类型:', type(s))
+        # print('文件类型:', type(s))
+        # str = s.text
+        # print("查看相应结果:", str)
+        # verifyhash = '6a6c6edb';
+        resultStr = re.findall(r"verifyhash = '(.+?)';", s.text)[0]
+        # yaml方式处理接口依赖
+        verifyDict = {"verifyhash": resultStr}
+        ReadExtract().write_extract(verifyDict)
+        print(verifyDict)
+        sleep(3)
 
-        # 断言 响应信息 及 状态码
+        # print(ReadExtract().read_extract()["verifyhash"])
+        # 断言 响应信息 及 状态码assertEquals
         self.assertEquals("OK", s.reason)
         # self.assertEquals(expect_result, s.json()['message'])
         # 断言响应状态码
-        # self.assertEquals(201, s.status_code)
+        self.assertEquals(200, s.status_code)
         # self.assertEquals(status_code, s.status_code)
 
 
---
+++ tools/read_extract.py
@@ -0,0 +1,13 @@
+﻿import yaml
+
+
+class ReadExtract:
+    def read_extract(self):
+        with open('../tools/extract.yaml', encoding='utf-8') as f:
+            result = yaml.load(stream=f, Loader=yaml.FullLoader)
+            # dict对象
+            return result
+
+    def write_extract(self, data):
+        with open('../tools/extract.yaml', encoding='utf-8', mode='w') as f:
+            yaml.dump(data, stream=f, allow_unicode=True)
